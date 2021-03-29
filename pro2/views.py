f6d12f4a62cdaac3fa0ef0dc2e8ef440c5758585
Nimish Jain <nimishjain.005@gmail.com>
2021-03-29 23:08:56 +0530

    First Commit

---
+++ pro2/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import os
+
+filename = os.path.abspath(__file__)
+dirname = os.path.dirname(filename)
+parentfile = os.path.dirname(dirname)
+
+
+def home(request):
+    return HttpResponse("<h1>Hello World</h1>")
+
+
+def rend_demo(request):
+    return render(request, "sample.html")
+
+
+def sam_demo(request):
+    return render(request, "html_demo/sample.html")
